395f8edd0caa4e769f254bb5a8b1d6bd7e910ffc
elro737 <eleanor.roger@student.unimelb.edu.au>
2026-03-06 14:35:57 +1100

    Added basis for website. Pages, navbar and aura additions. Next to do text and fix minor layouts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Eleanor Roger</h1>
-      <h2>Software Developer</h2>
+    <div>
 
-      <p>
-        Welcome to my portfolio website!
-      </p>
+      <nav className="navbar">
+        <a href="#Home">Home</a>
+        <a href="#About">About</a>
+        <a href="#Projects">Projects</a>
+        <a href="#Contact">Contact</a>
+      </nav>
 
-      <button onClick={() => window.open("https://github.com/El-Rog")}>
-        View My Github
-      </button>
+      <section id="Home" className="section green left">
+        <h1>Hi! I'm Eleanor Roger.</h1>
+        <h2>Welcome to my website!</h2>
+        <p>My fun place to play around and test out my skills. </p>
+      </section>
+
+      <section id="About" className="section purple right">
+        <h1>About Me</h1>
+      </section>
+
+      <section id="Projects" className="section red left">
+        <h1>Projects</h1>
+      </section>
+
+      <section id="Contact" className="section green right">
+        <h1>Contact</h1>
+      </section>
+
     </div>
-    
   );
 }
 
-export default App
+export default App;
